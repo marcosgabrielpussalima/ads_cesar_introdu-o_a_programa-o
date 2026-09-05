7812038093ae235e42c4406af86af9340b01cccd
Marcos <marcosgabrielpussalima@gmail.com>
2026-09-04 21:22:04 -0300

    exercicio 2

--- exercicios_condicionais.py
+++ exercicios_condicionais.py
@@ -1,6 +1,14 @@
+'''
 nome = input("digite o seu número")
 idade = input("digite o sua idade")
 if(idade>18):
     print("Menor de idade")
 else: 
     print("Maior de idade")
+'''
+#exercico 2
+numero = int(input("digite um número: "))
+if numero % 2 == 1:
+    print("Seu número é impar")
+else:
+   print("Seu número é par")
